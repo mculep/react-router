95dcb85a8a274457295bea82d1693e8b2ece8e4d
mculep <mculep@gmail.com>
2021-01-28 08:11:30 -0800

    react router demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Stuff from './components/Stuff'
+
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+        <header>
+          <Link to="/">Home</Link>
+          <Link to="/stuff">Stuff</Link>
+          <Link to="/about">About</Link>
+        </header>
+
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+
+          <Route path="/stuff">
+            <Stuff />
+          </Route>
+
+          <Route path="/About">
+            <About />
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Stuff.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Stuff = (props) => {
+    return (
+        <div>
+            <h1>Stuff</h1>
+        </div>
+    )
+}
+
+export default Stuff
